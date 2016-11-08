3797fd32df0ae7120a95c3f6d6a76aaa07712460
bcaccia <reverbtank@gmail.com>
2016-11-08 12:54:18 -0500

    Commit 11/8/16
    -Added in all applications that I want launched on run.
    -Added text so there is visual feedback of what is being launched.
    -Added in code so that the user directory on Windows is automatically
    detected so more relative paths can be used for things located in the
    user directory.

--- src/MorningStartupApplications.java
+++ src/MorningStartupApplications.java
@@ -2,18 +2,48 @@
  * Based on the code from: 
  * http://www.programmingsimplified.com/java/source-code/java-program-open-notepad
  */
-import java.util.*;
+import java.util.concurrent.TimeUnit; // Imported to use sleep function for adding delay between apps launching
 import java.io.*;
  
 class MorningStartupApplications {
   public static void main(String[] args) {
     Runtime rs = Runtime.getRuntime();
+    // Get the users current home directory
+    String currentUsersHomeDir = System.getProperty("user.home");
  
     try {
+      
+      // Outlook
+      rs.exec("C:\\Program Files\\Microsoft Office\\root\\Office16\\OUTLOOK.exe");
+      System.out.println("Launching Outlook...");
+      TimeUnit.SECONDS.sleep(10);
+      
+      // Mattermost
+      rs.exec(currentUsersHomeDir + "\\AppData\\Local\\mattermost\\app-1.3.0\\Mattermost.exe");
+      System.out.println("Launching Mattermost...");
+      TimeUnit.SECONDS.sleep(10);
+      
+      // Skype
+      rs.exec("C:\\Program Files (x86)\\Skype\\Phone\\Skype.exe");
+      System.out.println("Launching Skype...");
+      TimeUnit.SECONDS.sleep(10);
+      
+      // Notepad++
       rs.exec("C:\\Program Files (x86)\\Notepad++\\notepad++.exe");
+      System.out.println("Launching Notepad++...");
+      TimeUnit.SECONDS.sleep(10);
+      
+      // Chrome
+      rs.exec("C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe");
+      System.out.println("Launching Google Chrome...");
+      TimeUnit.SECONDS.sleep(10);
     }
+    // Catch any exceptions
     catch (IOException e) {
       System.out.println(e);
-    }   
+    } catch (InterruptedException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}   
   }
 }
